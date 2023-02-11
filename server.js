e6424da0143e57b7d83e1e2ed9836185032bbd57
Casey Glidewell <caseyglidewell@gmail.com>
2023-02-11 14:52:22 -0800

    hooked front and back end together, hooked db and backend together, added seed values, confirmed handshake to db is working properly.

--- server.js
+++ server.js
@@ -3,16 +3,29 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
+const getBooks = require('./book/getBooks')
 const app = express();
 app.use(cors());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
+// Establishing connection with atlas DB with URL
+mongoose.connect(process.env.MONGODB_URL);
 
-  response.send('test request received')
+// Assigning connection to variable to easily access mongoose connection methods
+const db = mongoose.connection;
 
-})
+// Listener for any errors, will print out error
+db.on('error', console.error.bind(console, 'connection error'));
+// Runs on 'open' will Console log connected
+db.once('open', () => console.log('Mongoose is connected'));
+
+
+// Default Route for server checking
+app.get('/', (req,res) => res.status(200).send('Default Route Working'));
+
+// Route that runs our getBooks function that was imported in
+app.get('/books', getBooks);
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
